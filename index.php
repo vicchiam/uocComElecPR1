530aee938835a00a825b74ebad3922a116250318
Victor Chisvert <vicchiam81@gmail.com>
2020-10-08 20:04:33 +0200

    Structure init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link href="https://fonts.googleapis.com/css?family=Shrikhand" rel="stylesheet">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="libs/css/bootstrap/bootstrap.min.css">
+
+        <link rel="stylesheet" href="libs/css/style.css">
+
+        <script src="libs/js/jquery/jquery.min.js" ></script>
+
+        <title>La Tiendecilla</title>
+    </head>
+    <body>
+        <?php include('php/menu.php'); ?>
+        <div id="main">
+            <?php include('php/index.php'); ?>
+        </div>
+        <script src="libs/js/popper/popper.min.js" ></script>
+        <script src="libs/js/bootstrap/bootstrap.min.js" ></script>
+    </body>
+</html>
---
+++ php/productos.php
@@ -0,0 +1,3 @@
+<div class="container">
+	<h1>Productos</h1>
+</div>
